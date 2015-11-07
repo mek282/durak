b0f850dc2582c5915a9f8a9010bd2d972bbf2e4a
Drew Samuels <drs354@cornell.edu>
2015-11-07 16:57:43 -0500

    Wrote mli file for AI responses

---
+++ ai.mli
@@ -0,0 +1,18 @@
+open Game
+open Cards
+
+(*Gives an AI's response to a first attack, in which it may have the
+* option to take, pass, or deflect*)
+val first_attack_response : state -> card -> command
+
+(*Gives an AI's response to subsequent attacks, in which it no longer
+* has the option to deflect*)
+val attack_response : state -> card list -> command list
+
+(*When it is the AI's turn to launch an initial attack, returns the card
+* the AI will attack with.*)
+val attack : state -> player -> card
+
+(*When another player is being attacked by all players, returns the cards
+* the AI will use to attack.*)
+val sec_attack : state -> player -> card list
