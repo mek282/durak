dc56e44df418c5f1466825a25693d7fa8b95a0ab
Mary <mek282@cornell.edu>
2015-11-08 14:02:28 -0500

    added to ai.mli

--- ai.mli
+++ ai.mli
@@ -16,3 +16,7 @@ val attack : state -> player -> card
 (*When another player is being attacked by all players, returns the cards
 * the AI will use to attack.*)
 val sec_attack : state -> player -> card list
+
+(* When it is an AI's turn to make a move, returns the AI's move as a
+* command. *)
+val response : state -> command
