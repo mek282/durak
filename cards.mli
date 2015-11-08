fc3723465d922a9fa59caa90417e5421e2f49d5b
Mary <mek282@cornell.edu>
2015-11-08 14:18:58 -0500

    added fields to game state

--- cards.mli
+++ cards.mli
@@ -1,13 +1,25 @@
-
-
+(*** Types to be used in Durak ***)
 
 type suit = | Heart | Club | Spade | Diamond
 type player_state = | Human | CPU of int
 
 type card = suit * int
 type deck = card list
-type player = {state : player_state; hand : deck}
+type player = { state : player_state;
+                hand : deck
+              }
 
-(* attackers index 0 primary attacker index lst next to be attacked *)
-type state = {deck: deck; trump: suit; attackers: player list; defender: player; table: (card * card option) list}
+(* Attackers index 0 represents primary attacker. Last index represents the
+ * next player to be attacked.
+ * Table is a list of pairs where the first element is an "attacking card" and
+ * the second element is None or Some "defending card." *)
+type state = { deck: deck;
+               trump: suit;
+               attackers: player list;
+               defender: player;
+               table: (card * card option) list;
+               active: player;
+               discard: deck;
+               winners: player list;
+              }
 
