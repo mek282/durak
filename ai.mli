8b8371a5f537291922432b7083fb68ee1102e47b
Drew Samuels <drs354@cornell.edu>
2015-11-08 14:26:25 -0500

    edited ai.mli to reflect new definition of state

--- ai.mli
+++ ai.mli
@@ -3,22 +3,6 @@ open Cards
 
 (*** Calculate an AI player's moves in response to current game state ***)
 
-(*Gives an AI's response to a first attack, in which it may have the
-* option to take, pass, or deflect*)
-val first_attack_response : state -> command
-
-(*Gives an AI's response to subsequent attacks, in which it no longer
-* has the option to deflect*)
-val attack_response : state -> command list
-
-(*When it is the AI's turn to launch an initial attack, returns the card
-* the AI will attack with.*)
-val attack : state -> card
-
-(*When another player is being attacked by all players, returns the cards
-* the AI will use to attack.*)
-val sec_attack : state -> card list
-
 (* When it is an AI's turn to make a move, returns the AI's move as a
 * command. *)
 val response : state -> command
