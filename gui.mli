41e7fd87d1658dfa3352a9f70a0b4cdab8319339
Drew Samuels <drs354@cornell.edu>
2015-11-11 21:37:22 -0500

    specified draw

--- gui.mli
+++ gui.mli
@@ -3,4 +3,8 @@ open Cards
 
 (*** Draws text interface for Durak game state ***)
 
-val draw : state -> ()
+(* Uses text-based graphics to print the contents of the player's hand, the
+ * current attacks and defenses, the number of cards in each opponent's hand,
+ * and the number of cards left in the deck.  Below this display, the string
+ * is printed, and the user is prompted for a response, which is returned. *)
+val draw : state -> string -> string
