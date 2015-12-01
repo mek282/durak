363972216673ce2544c95d88c86c2cbf2ecc1521
Drew Samuels <drs354@cornell.edu>
2015-11-30 23:22:24 -0500

    improved repl specification

--- game.mli
+++ game.mli
@@ -2,7 +2,8 @@ open Cards
 
 (*** The main game engine for Durak ***)
 
-(* The game engine *)
+(* The game engine. [repl g c s] will print message s to the human player and
+ * update g with command c, then recurse until a "durak" has been chosen *)
 val repl : state -> command -> string -> unit
 
 (* Interprets user inputs as commands *)
